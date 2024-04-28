608e144d48f00eb097c86086f7258d27baa2b867
Danilo Aprigio <danilo.cavalcante@live.com>
2024-04-28 12:44:45 -0300

    first commit

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# credentials.py
+
+USER_EMAIL = "casalea"
+USER_PASSWORD = "1234"
+
+# URL base da API
+BASE_URL_API = "http://api.sefaz.al.gov.br/sfz-economiza-alagoas-api/api/public/produto/pesquisa"
+# Token de acesso fornecido pela SEFAZ/AL
+APP_TOKEN = "e58ba7b6df2c658d6f9c47768053b79a8b540678"
